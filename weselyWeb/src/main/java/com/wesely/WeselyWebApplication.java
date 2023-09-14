58abfd45aae73487155468ff448f56400d16ed1d
guruma99 <bowbow_ovo@naver.com>
2023-09-14 13:56:37 +0900

    first commit!

---
+++ weselyWeb/src/main/java/com/wesely/WeselyWebApplication.java
@@ -0,0 +1,13 @@
+package com.wesely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeselyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeselyWebApplication.class, args);
+	}
+
+}
---
+++ weselyWeb/src/test/java/com/wesely/WeselyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeselyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
